581a4b105a00ec2655c59d13ea1b83a6ed114bb2
JamInTheJukebox <44655457+JamInTheJukebox@users.noreply.github.com>
2021-09-14 19:47:51 -0500

    New Project

---
+++ Assets/Scenes/GlobalTimer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GlobalTimer : MonoBehaviour
+{
+    [Header("The Counter")][Tooltip("I am the text")]
+    [SerializeField] TextMeshProUGUI CounterText;
+
+    private int m_counter = 0;
+    public int counter
+    {
+        get { return m_counter; }
+        set
+        {
+            if(m_counter != value)
+            {
+                m_counter = value;
+                CounterText.text = m_counter.ToString();
+            }
+        }
+    }
+
+}
---
+++ Assets/Scenes/Movement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float Speed; 
+    private void Awake()
+    {
+        print("Awake");
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(countEverySecond());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontalSpeed = 0;
+        if (Input.GetKey(KeyCode.D) | Input.GetKey(KeyCode.A))
+        {
+            horizontalSpeed = (Input.GetKey(KeyCode.D)) ? Speed : Speed * -1;
+        }
+        // rb.velocity = Input.GetAxis("horizontal") * Speed;
+        rb.velocity = new Vector2(horizontalSpeed, rb.velocity.y);
+    }
+
+    public IEnumerator countEverySecond()
+    {
+        int counter = 0;
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            print(counter++);
+            if(counter > 100)
+            {
+                break;
+            }
+            FindObjectOfType<GlobalTimer>().counter = counter;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        
+    }
+
+}
